28828a90c09822c3147bc3d3f148c6092ab9d332
Bulut Üzer <bulutuzer@gmail.com>
2013-09-09 14:36:22 +0300

    Slider'a ait id alını güncelemesi
    Slider'a ait id alını güncelemesi

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="slider marginAuto overflowHid">
-	<?php if( function_exists('cyclone_slider') ) cyclone_slider('12'); ?>
+	<?php if( function_exists('cyclone_slider') ) cyclone_slider('my-id'); ?>
 </div>
 
 <?php get_footer(); ?>
